881c129d6a4bc9bfd63e535f569903543bdd7431
jtarga <jtargui@gmail.com>
2017-10-31 09:00:15 +0100

    update files

--- Jenkinsfile
+++ Jenkinsfile
@@ -20,4 +20,12 @@ node {
         app.withRun("-P --net=host -p 127.0.0.1:5432:5432 --name keystoneesp_instance") {
         }
     }
+
+    stage('Test') {
+        app.inside {
+            sh 'npm test'
+        }
+    }
+
+
 }
---
+++ tests/simple_tests.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+var expect = chai.expect;
+//var index = require('../../cms/routes/index');
+
+describe('Simple test', function() {
+    it("Simple test", function() {
+        expect("25250").to.equal("25250");
+    });
+});
