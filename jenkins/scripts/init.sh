3584cf7450f24b4cff47d7e56ba3f961209cb49a
jtarga <jtargui@gmail.com>
2017-10-31 12:38:50 +0100

    update files

--- jenkins/scripts/init.sh
+++ jenkins/scripts/init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 npm install
-MOCHA_FILE=./jenkins-test-results.xml ./node_modules/.bin/mocha tests/** --reporter mocha-junit-reporter
+MOCHA_FILE=./jenkins-test-results.xml /var/jenkins/home/workspace/keystoneesp/node_modules/.bin/mocha tests/** --reporter mocha-junit-reporter
