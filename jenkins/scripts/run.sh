3db0d75d07e451f84e3510d63f1e47fee4bb4c85
jtarga <jtargui@gmail.com>
2017-10-31 14:48:04 +0100

    update files

--- jenkins/scripts/run.sh
+++ jenkins/scripts/run.sh
@@ -3,9 +3,8 @@ IMAGE_NAME="keystoneesp_image"
 VOLUME_NAME="keystoneesp_volume"
 DOCKER_INSTANCE_NAME="keystoneesp_instance"
 
-set -x
-docker pull keystoneesp
-set +x
-set -x
-docker run --rm -P --net=host -p 127.0.0.1:5432:5432 --name $DOCKER_INSTANCE_NAME keystoneesp
-set +x
+echo 'To Dev'
+#set -x
+#docker pull keystoneesp
+#docker run --rm -P --net=host -p 127.0.0.1:5432:5432 --name $DOCKER_INSTANCE_NAME keystoneesp
+#set +x
